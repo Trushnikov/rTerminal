f05062e636da6fc02a7f56b381e07072ad04819c
AT <alexei.trushnikov@gmail.com>
2025-01-20 20:59:25 +0300

    change linux installer create script

---
+++ rTerminal/app/linux/create_app_image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -Rf rTerminal
+mv ../../target/rTerminal-x-jar-with-dependencies.jar ../../target/app.jar
+jpackage --name rTerminal --main-jar app.jar --type app-image --input ../../target/ --icon ../../icon.png
