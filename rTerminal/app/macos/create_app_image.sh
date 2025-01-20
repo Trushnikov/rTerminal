18570ae2524e4373d7a4228f28f6b11ed948338b
Alexei Trushnikov <alexei.trushnikov@gmail.com>
2025-01-20 20:45:40 +0300

    gitignore is modified

--- rTerminal/app/macos/create_app_image.sh
+++ rTerminal/app/macos/create_app_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm rTerminal-1.0.app
-mv ../../target/terminal-x-jar-with-dependencies.jar ../../target/app.jar 
+mv ../../target/rTerminal-x-jar-with-dependencies.jar ../../target/app.jar 
 jpackage --name rTerminal --main-jar app.jar --type app-image --input ../../target/ --icon ../../icon.icns
 
